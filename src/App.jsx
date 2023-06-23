79727d0de3c49ef6b3555d10ff8d547f193fc73d
Isaac Rossi <isaacjohnrossi@gmail.com>
2023-06-23 14:23:49 +1000

    set up eslint

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import * as React from 'react'
-import './App.css'
+import * as React from "react";
+import "./App.css";
 
+const title = "React";
 function App() {
   return (
     <div>
-      <h1>Hello react</h1>
+      <h1>Hello World</h1>
+      <label htmlFor="search"> Search: </label>
+      <input id="search" type="text" />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;

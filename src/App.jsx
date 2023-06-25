a4fed166ec85d2b8a576623d51744c7d24cffaac
Isaac Rossi <isaacjohnrossi@gmail.com>
2023-06-25 17:25:58 +1000

    created a List and Search component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,59 @@
 import * as React from "react";
 import "./App.css";
 
-const title = "React";
+const list = [
+  {
+    title: "React",
+    url: "https://reactjs.org/",
+    author: "Jordan Walke",
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: "Redux",
+    url: "https://redux.js.org/",
+    author: "Dan Abramov, Andrew Clark",
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 function App() {
   return (
     <div>
-      <h1>Hello World</h1>
-      <label htmlFor="search"> Search: </label>
-      <input id="search" type="text" />
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List />
     </div>
   );
+}
+
+function List() {
+  return (
+    <ul>
+      {list.map((item) => {
+        return (
+          <li key={item.id}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </li>
+        );
+      })}
+    </ul>
+  );
+}
+
+function Search() {
+  <div>
+    <label htmlFor="search"> Search: </label>
+    <input id="search" type="text" />
+  </div>;
 }
 
 export default App;

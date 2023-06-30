d302c51f1ec97d99d36e4e20653fcb57d678b510
Isaac Rossi <isaacjohnrossi@gmail.com>
2023-06-30 16:08:33 +1000

    created a custom hook for local storage

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,18 @@ import * as React from "react";
 import "./App.css";
 
 const App = () => {
+  const useStorageState = (key, initialState) => {
+    const [value, setValue] = React.useState(
+      localStorage.getItem(key) || initialState
+    );
+
+    React.useEffect(() => {
+      localStorage.setItem(key, value);
+    }, [value, key]);
+
+    return [value, setValue];
+  };
+
   const stories = [
     {
       title: "React",
@@ -21,13 +33,7 @@ const App = () => {
     },
   ];
 
-  const [searchTerm, setSearchTerm] = React.useState(
-    localStorage.getItem("search") || "React"
-  );
-
-  React.useEffect(() => {
-    localStorage.setItem("search", searchTerm);
-  }, [searchTerm]);
+  const [searchTerm, setSearchTerm] = useStorageState("search", "React");
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);

d6e02da4a147c4bb49b176cc9e793d3860af2fad
Isaac Rossi <isaacjohnrossi@gmail.com>
2023-07-10 17:53:53 +1000

    made the data fetching explicit by adding in a button to confirm the search term

--- src/App.jsx
+++ src/App.jsx
@@ -46,8 +46,12 @@ const storiesReducer = (state, action) => {
   }
 };
 
+const API_ENDPOINT = `https://hn.algolia.com/api/v1/search?query=`;
+
 const App = () => {
   const [searchTerm, setSearchTerm] = useStorageState("search", "React");
+
+  const [url, setUrl] = React.useState(`${API_ENDPOINT}${searchTerm}`);
 
   const [stories, dispatchStories] = React.useReducer(storiesReducer, {
     data: [],
@@ -56,11 +60,9 @@ const App = () => {
   });
 
   const handleFetchStories = React.useCallback(() => {
-    if (!searchTerm) return;
-
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    fetch(`https://hn.algolia.com/api/v1/search?query=${searchTerm}`)
+    fetch(url)
       .then((response) => response.json())
       .then((result) => {
         dispatchStories({
@@ -71,7 +73,7 @@ const App = () => {
       .catch(() => {
         dispatchStories({ type: "STORIES_FETCH_FAILURE" });
       });
-  }, [searchTerm]);
+  }, [url]);
 
   React.useEffect(() => {
     handleFetchStories();
@@ -84,8 +86,12 @@ const App = () => {
     });
   };
 
-  const handleSearch = (event) => {
+  const handleSearchInput = (event) => {
     setSearchTerm(event.target.value);
+  };
+
+  const handleSearchSubmit = () => {
+    setUrl(`${API_ENDPOINT}${searchTerm}`);
   };
 
   return (
@@ -95,11 +101,14 @@ const App = () => {
         id="search"
         label="search"
         isFocused
-        onInputChange={handleSearch}
+        onInputChange={handleSearchInput}
         value={searchTerm}
       >
         <strong>Search:</strong>
       </InputWithLabel>
+      <button type="button" onClick={handleSearchSubmit} disabled={!searchTerm}>
+        Confirm
+      </button>
       <hr />
 
       {stories.isError && <p>Something went wrong...</p>}

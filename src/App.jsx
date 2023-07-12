405ad203b7877121468dc8915d02d5daa7a74e8f
Isaac Rossi <isaacjohnrossi@gmail.com>
2023-07-12 14:04:36 +1000

    replaced native fetch with axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import axios from "axios";
 import "./App.css";
 
 const useStorageState = (key, initialState) => {
@@ -62,12 +63,12 @@ const App = () => {
   const handleFetchStories = React.useCallback(() => {
     dispatchStories({ type: "STORIES_FETCH_INIT" });
 
-    fetch(url)
-      .then((response) => response.json())
+    axios
+      .get(url)
       .then((result) => {
         dispatchStories({
           type: "STORIES_FETCH_SUCCESS",
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() => {
